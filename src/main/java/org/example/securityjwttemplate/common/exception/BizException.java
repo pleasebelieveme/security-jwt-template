a385ce99ac63d141f35d8d3de8a5242f4525f0fa
believeme <cain1227@naver.com>
2025-11-15 16:11:00 +0900

    fix: 필요없는 메서드 정리

--- src/main/java/org/example/securityjwttemplate/common/exception/BizException.java
+++ src/main/java/org/example/securityjwttemplate/common/exception/BizException.java
@@ -9,16 +9,8 @@ public class BizException extends RuntimeException {
 
 	private final ResponseCode responseCode;
 
-	public BizException(String message, ResponseCode responseCode) {
-		super(message);
-		this.responseCode = responseCode;
-	}
-
 	public BizException(ResponseCode responseCode) {
 		super(responseCode.getMessage());
 		this.responseCode = responseCode;
 	}
-
-	/* 3. getStatus(), getCode(), getErrorMessage() 메서드는 제거됨 */
-	// GlobalExceptionHandler에서 exception.getResponseCode().getStatus() 등으로 접근 가능합니다.
 }
